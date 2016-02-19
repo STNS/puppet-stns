01e9c4f9d720d4861c9dd44a8abddd9c845c4595
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-19 22:23:58 +0900

    Divide nodesets

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -3,16 +3,14 @@ require 'spec_helper_acceptance'
 describe 'stns class' do
   let(:manifest) {
     <<-EOS
-    'include ::stns'
+      include ::stns
     EOS
   }
 
-  hosts.each do |host|
-    it "should work without errors on #{host}" do
-      result = apply_manifest_on(host, manifest, :acceptable_exit_codes => [0, 2], :catch_failures => true)
+  it "should work without errors" do
+    result = apply_manifest(manifest, :acceptable_exit_codes => [0, 2], :catch_failures => true)
 
-      expect(result.exit_code).not_to eq 4
-      expect(result.exit_code).not_to eq 6
-    end
+    expect(result.exit_code).not_to eq 4
+    expect(result.exit_code).not_to eq 6
   end
 end
