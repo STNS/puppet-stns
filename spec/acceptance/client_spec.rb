bbd02d239ec073f724562f80899d4cca838a3ea0
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-20 03:56:36 +0900

    It should run a second time without changes

--- spec/acceptance/client_spec.rb
+++ spec/acceptance/client_spec.rb
@@ -9,9 +9,13 @@ describe 'stns::client class' do
 
   it "should work without errors" do
     result = apply_manifest(manifest, :acceptable_exit_codes => [0, 2], :catch_failures => true)
-
     expect(result.exit_code).not_to eq 4
     expect(result.exit_code).not_to eq 6
+  end
+
+  it "should run a second time without changes" do
+    result = apply_manifest(manifest)
+    expect(result.exit_code).to eq 0
   end
 
   describe package('libnss-stns') do
