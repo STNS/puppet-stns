82ec77492bc6b8a7284b25c2b5bb094dbd56ac68
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-20 03:31:43 +0900

    Set defaults

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -3,7 +3,15 @@
 #
 # stns::client::config is to configure libnss-stns.
 
-class stns::client::config {
+class stns::client::config (
+  $api_end_point     = $stns::client::api_end_point,
+  $user              = $stns::client::user,
+  $password          = $stns::client::password,
+  $wrapper_path      = $stns::client::wrapper_path,
+  $chain_ssh_wrapper = $stns::client::chain_ssh_wrapper,
+  $ssl_verify        = $stns::client::ssl_verify,
+)
+{
 
   file { '/etc/stns/libnss_stns.conf':
     ensure  => present,
