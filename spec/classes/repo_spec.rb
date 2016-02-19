24804c4d48b56372b727ccd670357f82b12f02ed
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-20 01:08:24 +0900

    Add repo spec

---
+++ spec/classes/repo_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+describe 'stns::repo' do
+  context 'with defaults for all parameters' do
+    it { is_expected.to compile }
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_class('stns::repo') }
+  end
+end
