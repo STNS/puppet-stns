a396741753e8396e17b3f04bb42cff0fbcefb295
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-29 02:40:15 +0900

    Mock of stns::server::groups and stns::server::users

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -5,17 +5,3 @@ describe 'stns::server' do
     it { is_expected.to contain_class('stns::server') }
   end
 end
-
-describe 'stns::server::users' do
-  context 'with defaults for all parameters' do
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_class('stns::server::users') }
-  end
-end
-
-describe 'stns::server::groups' do
-  context 'with defaults for all parameters' do
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_class('stns::server::groups') }
-  end
-end
