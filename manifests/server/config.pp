eaef78988192095be84fae5108ded6fd89f3f734
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-28 19:13:26 +0900

    Change class params to vars

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -3,13 +3,13 @@
 #
 # stns::server::config is to configure stns.
 
-class stns::server::config (
-  $port     = $stns::server::port,
-  $user     = $stns::server::user,
-  $password = $stns::server::password,
-  $users    = $stns::server::users,
-  $groups   = $stns::server::groups,
-) {
+class stns::server::config {
+
+  $port     = $stns::server::port
+  $user     = $stns::server::user
+  $password = $stns::server::password
+  $users    = $stns::server::users
+  $groups   = $stns::server::groups
 
   file { '/etc/stns/stns.conf':
     ensure  => present,
