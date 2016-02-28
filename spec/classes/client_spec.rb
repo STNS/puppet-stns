f40805f95e8a9103787cb538252c1cc988f9140f
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-28 18:52:50 +0900

    Remove dups

--- spec/classes/client_spec.rb
+++ spec/classes/client_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 describe 'stns::client' do
   context 'with defaults for all parameters' do
-    it { is_expected.to compile }
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_class('stns::client') }
   end
--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 describe 'stns::repo' do
   context 'with defaults for all parameters' do
-    it { is_expected.to compile }
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_class('stns::repo') }
   end
