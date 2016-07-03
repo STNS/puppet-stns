8be98600752d35069ff582df6c9dca0431474130
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-07-03 12:05:05 +0900

    Expect exit status equal 2

--- spec/acceptance/client_spec.rb
+++ spec/acceptance/client_spec.rb
@@ -35,9 +35,8 @@ describe 'stns::client class' do
   end
 
   it 'should work without errors' do
-    result = apply_manifest(manifest, acceptable_exit_codes: [0, 2], catch_failures: true)
-    expect(result.exit_code).not_to eq 4
-    expect(result.exit_code).not_to eq 6
+    result = apply_manifest(manifest, catch_failures: true)
+    expect(result.exit_code).to eq 2
   end
 
   it 'should run a second time without changes' do
