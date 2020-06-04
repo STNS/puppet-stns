413c129aa7cc001823b147935c3c9224e39d55bf
Yuki Koya <ykky@pepabo.com>
2020-06-04 13:32:42 +0900

    Add a special URL for Ubuntu
    see. https://github.com/STNS/libnss/issues/32

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -1,7 +1,17 @@
 class stns::repo::apt {
+  if $::os['name'] == 'Ubuntu' {
+    $codename = $::os['distro']['codename']
+    $location = "https://repo.stns.jp/${codename}/"
+    $repos    = $codename
+  } else {
+    $location = "https://repo.stns.jp/debian/"
+    $repos    = 'main'
+  }
+
   apt::source { 'stns':
-    location => 'http://repo.stns.jp/debian/',
+    location => $location,
     release  => 'stns',
+    repos    => $repos,
     key      => {
       id     => '6BACCE33697C7E568D5C162F018A7A21B2EC51BA',
       source => $::stns::repo::gpgkey_url,
