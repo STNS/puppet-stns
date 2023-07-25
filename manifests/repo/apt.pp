5c9732713e35a2561d0bf89f1f4e056bd5073e74
sugy <sugy.kz@gmail.com>
2023-07-25 15:47:06 +0900

    setup apt source uri.
    - https://github.com/STNS/package/blob/master/assets/scripts/apt-repo.sh#L14-L15

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -1,7 +1,9 @@
 class stns::repo::apt {
+  $dist = $::facts['lsbdistcodename']
   apt::source { 'stns':
-    location => 'http://repo.stns.jp/debian/',
+    location => "http://repo.stns.jp/${dist}/",
     release  => 'stns',
+    repos    => $dist,
     key      => {
       id     => 'ED9008B740C6735CB3EF098C37DE344F75E258B6',
       source => $::stns::repo::gpgkey_url,
